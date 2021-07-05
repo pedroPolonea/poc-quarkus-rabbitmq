0ff1cbb9a4a3ce5879b284f483f5fbc8bc597670
pedroPolonea <phpolonea@hotmail.com>
2021-07-05 14:27:23 -0300

    Init

---
+++ src/main/java/org/acme/resteasyjackson/MovieResource.java
@@ -0,0 +1,25 @@
+package org.acme.resteasyjackson;
+
+import javax.inject.Inject;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+@Path("/")
+@Consumes(MediaType.WILDCARD)
+@Produces(MediaType.WILDCARD)
+public class MovieResource {
+
+    @Inject
+    MessageService producer;
+
+    @POST
+    public Response send(String movie) {
+        producer.send(movie);
+        // Return an 202 - Accepted response.
+        return Response.accepted().build();
+    }
+}
